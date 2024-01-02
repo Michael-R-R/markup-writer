d4d66be587b26c4dc62e822fe0f3e2a4dd2034f3
Michael-R-R <mikerule68@gmail.com>
2024-01-02 09:59:02 -0700

    fixed updating viewport margins;

--- markupwriter/widgets/document_editor.py
+++ markupwriter/widgets/document_editor.py
@@ -34,19 +34,26 @@ class DocumentEditor(QPlainTextEdit):
         self.setFrameShape(QFrame.Shape.NoFrame)
         self.setWordWrapMode(QTextOption.WrapMode.WordWrap)
         self.setTabStopDistance(20.0)
-        self.setViewportMargins(1, 1, 1, 1)
+        self.updateViewportMargins()
         self.setStyleSheet(Style.EDITOR)
 
-    def updateMargins(self):
-        mW = QGuiApplication.primaryScreen().size().width()
+    def updateViewportMargins(self):
+        mSize = QGuiApplication.primaryScreen().size()
+        mW = mSize.width()
 
         wW = self.width()
-        wW = int(wW * 0.1) if wW < int(mW / 2) else int(wW * 0.3)
+        if wW > int(mW * 0.75):
+            wW = int(wW * 0.3)
+        elif wW > int(mW * 0.5):
+            wW = int(wW * 0.2)
+        else:
+            wW = int(wW * 0.1)
+            
         wH = int(self.height() * 0.1)
 
         self.setViewportMargins(wW, wH, wW, wH)
 
     def resizeEvent(self, e: QResizeEvent | None) -> None:
         AppConfig.docEditorSize = e.size()
-        self.updateMargins()
+        self.updateViewportMargins()
         super().resizeEvent(e)
