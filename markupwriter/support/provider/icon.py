ae50dcbaf6530666500a945202aa6816f4abff6d
Michael-R-R <mikerule68@gmail.com>
2024-01-03 12:04:45 -0700

    reorganized icons;

--- markupwriter/support/provider/icon.py
+++ markupwriter/support/provider/icon.py
@@ -11,27 +11,27 @@ from PyQt6.QtGui import (
 class Icon(object):
     QDir.addSearchPath("icons", "./resources/icons/")
     
+    # Common
+    CHECK = QIcon("icons:common/check.svg")
+    UNCHECK = QIcon("icons:common/uncheck.svg")
+    UP_ARROW = QIcon("icons:common/up_arrow.svg")
+    DOWN_ARROW = QIcon("icons:common/down_arrow.svg")
+    ADD_ITEM = QIcon("icons:common/add_item.svg")
+    MORE_OPTIONS = QIcon("icons:common/more_options.svg")
+    
     # Folders
-    NOVEL_FOLDER = QIcon("icons:novel.svg")
-    PLOT_FOLDER = QIcon("icons:plot.svg")
-    TIMELINE_FOLDER = QIcon("icons:timeline.svg")
-    CHARACTERS_FOLDER = QIcon("icons:characters.svg")
-    LOCATIONS_FOLDER = QIcon("icons:locations.svg")
-    OBJECTS_FOLDER = QIcon("icons:objects.svg")
-    TRASH_FOLDER = QIcon("icons:trash.svg")
-    MISC_FOLDER = QIcon("icons:misc_folder.svg")
+    NOVEL_FOLDER = QIcon("icons:folder/novel.svg")
+    PLOT_FOLDER = QIcon("icons:folder/plot.svg")
+    TIMELINE_FOLDER = QIcon("icons:folder/timeline.svg")
+    CHARACTERS_FOLDER = QIcon("icons:folder/characters.svg")
+    LOCATIONS_FOLDER = QIcon("icons:folder/locations.svg")
+    OBJECTS_FOLDER = QIcon("icons:folder/objects.svg")
+    TRASH_FOLDER = QIcon("icons:folder/trash.svg")
+    MISC_FOLDER = QIcon("icons:folder/misc_folder.svg")
 
     # Files
-    TITLE_FILE = QIcon("icons:title_page.svg")
-    CHAPTER_FILE = QIcon("icons:chapter.svg")
-    SCENE_FILE = QIcon("icons:scene.svg")
-    SECTION_FILE = QIcon("icons:section.svg")
-    MISC_FILE = QIcon("icons:misc_file.svg")
-
-    # Misc
-    CHECK = QIcon("icons:check.svg")
-    UNCHECK = QIcon("icons:uncheck.svg")
-    UP_ARROW = QIcon("icons:up_arrow.svg")
-    DOWN_ARROW = QIcon("icons:down_arrow.svg")
-    ADD_ITEM = QIcon("icons:add_item.svg")
-    MORE_OPTIONS = QIcon("icons:more_options.svg")
+    TITLE_FILE = QIcon("icons:file/title_page.svg")
+    CHAPTER_FILE = QIcon("icons:file/chapter.svg")
+    SCENE_FILE = QIcon("icons:file/scene.svg")
+    SECTION_FILE = QIcon("icons:file/section.svg")
+    MISC_FILE = QIcon("icons:file/misc_file.svg")
