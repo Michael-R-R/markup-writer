ab44e91e83c8711a9e17f731b89a3e8528a3256d
Michael-R-R <mikerule68@gmail.com>
2024-01-20 13:53:53 -0700

    added refresh button

--- markupwriter/widgets/document_preview_widget.py
+++ markupwriter/widgets/document_preview_widget.py
@@ -3,6 +3,7 @@
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
     QTextEdit,
     QPushButton,
 )
@@ -31,27 +32,53 @@ class DocumentPreviewWidget(QWidget):
         self.textedit.setTabStopDistance(20.0)
         self.textedit.setPlainText(self.plainText)
         
+        self.refreshButton = QPushButton("Refresh", self)
+        self.refreshButton.clicked.connect(self._onRefreshButton)
+        
         self.toggleButton = QPushButton("Plain", self)
         self.toggleButton.clicked.connect(self._onToggleButton)
         
+        self.hLayout = QHBoxLayout()
+        self.hLayout.addWidget(self.refreshButton)
+        self.hLayout.addWidget(self.toggleButton)
+        
         self.vLayout = QVBoxLayout(self)
         self.vLayout.addWidget(self.textedit)
-        self.vLayout.addWidget(self.toggleButton)
+        self.vLayout.addLayout(self.hLayout)
+        
+    def _onRefreshButton(self):
+        path = AppConfig.projectContentPath() + self.uuid
+        if not File.exists(path):
+            self.close()
+            
+        text = File.read(path)
+        self.plainText = text
+        self.html = ""
+        if self.isPlainText:
+            self._setPlainText(text)
+        else:
+            self._setHtmlText(self.html)
         
     def _onToggleButton(self):
         self.isPlainText = not self.isPlainText
         
         if self.isPlainText:
-            self.toggleButton.setText("Plain")
-            self.textedit.setPlainText(self.plainText)
+            self._setPlainText(self.plainText)
         else:
-            self.toggleButton.setText("HTML")
-            self.textedit.clear()
-            if self.html == "":
-                tokenizer = PreviewTokenizer(self.plainText)
-                tokens = tokenizer.run()
-                parser = PreviewParser()
-                self.html = parser.run(tokens)
-                self.textedit.setHtml(self.html)
-            else:
-                self.textedit.setHtml(self.html)
+            self._setHtmlText(self.html)
+                
+    def _setPlainText(self, text: str):
+        self.plainText = text
+        self.toggleButton.setText("Plain")
+        self.textedit.setPlainText(text)
+        
+    def _setHtmlText(self, text: str):
+        if text == "":
+            tokenizer = PreviewTokenizer(self.plainText)
+            tokens = tokenizer.run()
+            parser = PreviewParser()
+            text = parser.run(tokens)
+            
+        self.html = text
+        self.toggleButton.setText("HTML")
+        self.textedit.setHtml(self.html)
