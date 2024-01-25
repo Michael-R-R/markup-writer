55d98eca50bb0fd5db5055b6b0943baf17fd3d85
Michael-R-R <mikerule68@gmail.com>
2024-01-25 12:25:09 -0700

    update to editor tokenizer

--- markupwriter/common/tokenizers/editor_tokenizer.py
+++ markupwriter/common/tokenizers/editor_tokenizer.py
@@ -19,38 +19,37 @@ class WorkerSignal(QObject):
 class EditorTokenizer(QRunnable):
     def __init__(self, uuid: str, text: str, parent: QObject | None) -> None:
         super().__init__()
-        
+
         lineRegex = r"^@(tag)(\(.*\))"
         tagRegex = r"^@(tag)"
         namesRegex = r"(?<=\().+?(?=\))"
-        
+
         self.uuid = uuid
         self.text = text
-        self.signals = WorkerSignal(parent)
         self.linePattern = re.compile(lineRegex, re.MULTILINE)
         self.tagPattern = re.compile(tagRegex)
         self.namesPattern = re.compile(namesRegex)
+        self.tokens: dict[str, list[str]] = {
+            "@tag": list(),
+        }
+        self.signals = WorkerSignal(parent)
 
     @pyqtSlot()
     def run(self):
         try:
-            tokens: dict[str, list[str]] = {
-                "@tag": list(),
-            }
-
             it = self.linePattern.finditer(self.text)
             for found in it:
                 line = found.group(0)
                 tag = self.tagPattern.search(line).group(0)
                 names = self.namesPattern.search(line).group(0)
-                
+
                 nameList = [n.strip() for n in names.split(",")]
-                
-                tokens[tag].append(nameList)
+
+                self.tokens[tag].append(nameList)
 
         except Exception as e:
             self.signals.error.emit(str(e))
 
         else:
             self.signals.finished.emit()
-            self.signals.result.emit(self.uuid, tokens)
+            self.signals.result.emit(self.uuid, self.tokens)
