c026f347c83eb4e94c45a972c22faee297d140c3
Michael-R-R <mikerule68@gmail.com>
2024-01-27 13:42:16 -0700

    updated search replace layout

--- markupwriter/widgets/search_replace_widget.py
+++ markupwriter/widgets/search_replace_widget.py
@@ -3,6 +3,7 @@
 import re
 
 from PyQt6.QtCore import (
+    Qt,
     pyqtSignal,
     pyqtSlot,
 )
@@ -14,6 +15,8 @@ from PyQt6.QtGui import (
 from PyQt6.QtWidgets import (
     QFrame,
     QGridLayout,
+    QSpacerItem,
+    QSizePolicy,
     QLineEdit,
     QLabel,
     QToolBar,
@@ -24,65 +27,71 @@ from markupwriter.common.provider import Icon
 
 
 class SearchReplaceWidget(QFrame):
-    def __init__(self, parent: QPlainTextEdit | None) -> None:
-        super().__init__(parent)
-        
-        self.textEdit = parent
-        
+    def __init__(self, textEdit: QPlainTextEdit | None) -> None:
+        super().__init__(textEdit)
+
+        self.textEdit = textEdit
+
         self.setAutoFillBackground(True)
         self.setFrameStyle(QFrame.Shape.StyledPanel | QFrame.Shadow.Plain)
-        
+
         self.searchInput = QLineEdit(self)
-        self.searchInput.setFixedHeight(20)
-        
+        self.searchInput.setContentsMargins(0, 0, 0, 0)
+        self.searchInput.setMaximumHeight(20)
+
         self.resultsLabel = QLabel("No results", self)
-        self.resultsLabel.setFixedHeight(20)
-        
+        self.resultsLabel.setContentsMargins(0, 0, 0, 0)
+        self.resultsLabel.setMaximumHeight(20)
+
         self.searchToolbar = QToolBar(self)
-        self.searchToolbar.setFixedHeight(20)
+        self.searchToolbar.setContentsMargins(0, 0, 0, 0)
+        self.searchToolbar.setMaximumHeight(20)
         self.prevAction = QAction(Icon.UP_ARROW, "Previous match", self)
         self.nextAction = QAction(Icon.DOWN_ARROW, "Next match", self)
         self.closeAction = QAction(Icon.UNCHECK, "Close", self)
         self.searchToolbar.addAction(self.prevAction)
         self.searchToolbar.addAction(self.nextAction)
         self.searchToolbar.addAction(self.closeAction)
-        
+
         self.replaceInput = QLineEdit(self)
-        self.replaceInput.setFixedHeight(20)
-        
+        self.replaceInput.setContentsMargins(0, 0, 0, 0)
+        self.replaceInput.setMaximumHeight(20)
+
         self.replaceToolbar = QToolBar(self)
-        self.replaceToolbar.setFixedHeight(20)
+        self.replaceToolbar.setContentsMargins(0, 0, 0, 0)
+        self.replaceToolbar.setMaximumHeight(20)
         self.replaceAction = QAction(Icon.PLACE_HOLDER, "Replace", self)
         self.replaceAllAction = QAction(Icon.PLACE_HOLDER, "Replace all", self)
         self.replaceToolbar.addAction(self.replaceAction)
         self.replaceToolbar.addAction(self.replaceAllAction)
-        
-        self.mLayout = QGridLayout(self)
-        self.mLayout.addWidget(self.searchInput, 0, 0)
-        self.mLayout.addWidget(self.resultsLabel, 0, 1)
-        self.mLayout.addWidget(self.searchToolbar, 0, 2)
-        self.mLayout.addWidget(self.replaceInput, 1, 0)
-        self.mLayout.addWidget(self.replaceToolbar, 1, 1)
-        
+
+        self.gLayout = QGridLayout(self)
+        self.gLayout.setVerticalSpacing(4)
+        self.gLayout.addWidget(self.searchInput, 0, 0)
+        self.gLayout.addWidget(self.resultsLabel, 0, 1)
+        self.gLayout.addWidget(self.searchToolbar, 0, 2)
+        self.gLayout.addWidget(self.replaceInput, 1, 0)
+        self.gLayout.addWidget(self.replaceToolbar, 1, 1)
+
         self.searchInput.textChanged.connect(self._onSearchChanged)
         self.closeAction.triggered.connect(lambda: self.hide())
-        
+
     def reset(self):
         self.hide()
         self.searchInput.clear()
         self.replaceInput.clear()
-        
+
     def toggle(self):
         if self.isVisible():
             self.hide()
         else:
             self.show()
             self.adjustPos()
-        
+
     def adjustPos(self):
         if not self.isVisible():
             return
-        
+
         vb = self.textEdit.verticalScrollBar()
         vbw = vb.width() if vb.isVisible() else 0
         ww = self.textEdit.width()
@@ -91,8 +100,9 @@ class SearchReplaceWidget(QFrame):
         x = ww - vbw - srw - 2 * fw
         y = 2 * fw
         self.move(x, y)
-        
+
     pyqtSlot()
+
     def _onSearchChanged(self, text: str):
         print(text)
         # TODO implement
