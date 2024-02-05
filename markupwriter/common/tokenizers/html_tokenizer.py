8165c33b07bde6414358f89033f680cd1c6e8e13
Michael-R-R <mikerule68@gmail.com>
2024-02-05 14:46:38 -0700

    updated tokenizing process

--- markupwriter/common/tokenizers/html_tokenizer.py
+++ markupwriter/common/tokenizers/html_tokenizer.py
@@ -11,30 +11,30 @@ class HtmlTokenizer(object):
 
         self.parenRegex = re.compile(r"(?<=\().*?(?=\))")
         self.nlParenRegex = re.compile(r"(?<=\()(\n|.)*?(?=\))")
+        self.tokenRegex = re.compile(r"^@.*(?=\()")
 
-        self.replaceFuncs = {
+        self.replaceDict = {
             r"@bold\((\n|.)*?\)": self._preprocessBold,
             r"@ital\((\n|.)*?\)": self._preprocessItal,
             r"@boldItal\((\n|.)*?\)": self._preprocessBoldItal,
-            r"@rename\(.*\)": self._preprocessRename,
-            r"@vspace\(.*\)": self._preprocessVSpace
         }
 
-        self.removeFuncs = {
+        self.removeDict = {
             r"^cpos:.*": self._preprocessRemove,
             r"^@(tag|ref|pov|loc)(\(.*\))": self._preprocessRemove,
             r"%.*": self._preprocessRemove,
             r"<#(\n|.)*?#>": self._preprocessRemove,
         }
 
-        self.processFuncs = {
-            r"^@title\(.*\)": self._processTitle,
-            r"^@chapter\(.*\)": self._processChapter,
-            r"^@scene\(.*\)": self._processScene,
-            r"^@section\(.*\)": self._processSection,
-            r"@alignL\(.*\)": self._processAlignL,
-            r"@alignC\(.*\)": self._processAlignC,
-            r"@alignR\(.*\)": self._processAlignR,
+        self.tokenDict = {
+            r"@title": self._processTitle,
+            r"@chapter": self._processChapter,
+            r"@scene": self._processScene,
+            r"@section": self._processSection,
+            r"@alignL": self._processAlignL,
+            r"@alignC": self._processAlignC,
+            r"@alignR": self._processAlignR,
+            r"@vspace": self._preprocessVSpace,
         }
 
     def run(self) -> str:
@@ -44,11 +44,11 @@ class HtmlTokenizer(object):
         return self.body
 
     def _preprocess(self):
-        for tag in self.replaceFuncs:
-            self.replaceFuncs[tag](tag)
+        for tag in self.replaceDict:
+            self.replaceDict[tag](tag)
             
-        for tag in self.removeFuncs:
-            self.removeFuncs[tag](tag)
+        for tag in self.removeDict:
+            self.removeDict[tag](tag)
 
     def _preprocessBold(self, tag: str):
         self._preprocessFormat(tag, "<b>?</b>")
@@ -80,34 +80,6 @@ class HtmlTokenizer(object):
                 
             self.text = self.text.replace(found, htmlText)
 
-    def _preprocessRename(self, tag: str):
-        it = re.finditer(tag, self.text, re.MULTILINE)
-        for found in it:
-            tag = found.group(0)
-            pair = self.nlParenRegex.search(tag)
-            if pair is None:
-                continue
-
-            pair = pair.group(0).strip().split(",")
-            if len(pair) != 2:
-                continue
-
-            self.text = self.text.replace(tag, pair[1])
-            self.text = self.text.replace(pair[0], pair[1])
-
-    def _preprocessVSpace(self, tag: str):
-        it = re.finditer(tag, self.text, re.MULTILINE)
-        for found in it:
-            found = found.group(0)
-            text = self.nlParenRegex.search(found)
-            text = None if text is None else text.group(0)
-            if text is None:
-                continue
-            if not text.isnumeric():
-                continue
-            htmlText = "<br>" * int(text)
-            self.text = self.text.replace(found, htmlText)
-
     def _preprocessRemove(self, tag: str):
         it = re.finditer(tag, self.text, re.MULTILINE)
         for found in it:
@@ -115,74 +87,81 @@ class HtmlTokenizer(object):
                 continue
             self.text = self.text.replace(found.group(0), "")
 
-    def _process(self) -> bool:
+    def _process(self):
         # Process line by line
         for line in self.text.splitlines():
             if line == "":
                 continue
 
-            isProcessed = False
-            for tag in self.processFuncs:
-                if self.processFuncs[tag](tag, line):
-                    isProcessed = True
-                    break
-            
-            if not isProcessed:
+            token = self.tokenRegex.search(line)
+            if token is not None:
+                token = token.group(0)
+                self.tokenDict[token](line)
+            else:
                 self._processParagraph(line)
 
-    def _processTitle(self, tag: str, text: str) -> bool:
-        found = re.search(tag, text)
+    def _processTitle(self, text: str) -> bool:
+        found = self.parenRegex.search(text)
         if found is None:
             return False
-        htmlText = self.parenRegex.search(found.group(0))
-        self.body += "<h1 class='title'>{}</h1>\n".format(htmlText.group(0))
+        htmlText = found.group(0)
+        self.body += "<h1 class='title'>{}</h1>\n".format(htmlText)
         return True
     
-    def _processChapter(self, tag: str, text: str) -> bool:
-        found = re.search(tag, text)
+    def _processChapter(self, text: str) -> bool:
+        found = self.parenRegex.search(text)
         if found is None:
             return False
-        htmlText = self.parenRegex.search(found.group(0))
-        self.body += "<h2 class='chapter'>{}</h2>\n".format(htmlText.group(0))
+        htmlText = found.group(0)
+        self.body += "<h2 class='chapter'>{}</h2>\n".format(htmlText)
         return True
     
-    def _processScene(self, tag: str, text: str) -> bool:
-        found = re.search(tag, text)
+    def _processScene(self, text: str) -> bool:
+        found = self.parenRegex.search(text)
         if found is None:
             return False
         self.body += "<p class='scene'><br>* * *<br></p>\n"
         return True
     
-    def _processSection(self, tag: str, text: str) -> bool:
-        found = re.search(tag, text)
+    def _processSection(self, text: str) -> bool:
+        found = self.parenRegex.search(text)
         if found is None:
             return False
         self.body += "<div class='section'><br><br></div>\n"
         return True
     
-    def _processAlignL(self, tag: str, text: str) -> bool:
-        return self._processAlign(tag, text, "<p class='alignL'>?</p>\n")
+    def _processAlignL(self, text: str) -> bool:
+        return self._processAlign(text, "<p class='alignL'>?</p>\n")
     
-    def _processAlignC(self, tag: str, text: str) -> bool:
-        return self._processAlign(tag, text, "<p class='alignC'>?</p>\n")
+    def _processAlignC(self, text: str) -> bool:
+        return self._processAlign(text, "<p class='alignC'>?</p>\n")
     
-    def _processAlignR(self, tag: str, text: str) -> bool:
-        return self._processAlign(tag, text, "<p class='alignR'>?</p>\n")
+    def _processAlignR(self, text: str) -> bool:
+        return self._processAlign(text, "<p class='alignR'>?</p>\n")
     
-    def _processAlign(self, tag: str, text: str, html: str) -> bool:
-        found = re.search(tag, text)
+    def _processAlign(self, text: str, html: str) -> bool:
+        found = self.parenRegex.search(text)
         if found is None:
             return False
         found = found.group(0)
-        
-        alignText = self.parenRegex.search(text)
-        if alignText is None:
-            return False
-        alignText = alignText.group(0)
 
-        html = html.replace("?", alignText)
+        html = html.replace("?", found)
         
         self.body += html
+        
+        return True
+    
+    def _preprocessVSpace(self, text: str) -> bool:
+        found = self.parenRegex.search(text)
+        if found is None:
+            return False
+        found = found.group(0)
+        if not found.isnumeric():
+            return False
+        
+        htmlText = "<br>" * int(found)
+        
+        self.body += "<p class='vspace'>{}</p>".format(htmlText)
         
         return True
     
