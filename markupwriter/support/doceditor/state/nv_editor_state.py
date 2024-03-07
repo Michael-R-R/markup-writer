ca81cf68ed7c73a22e6c2665e0fcf94cd3675444
Michael-R-R <mikerule68@gmail.com>
2024-03-07 13:36:10 -0700

    overhauling nv state to state machine

--- markupwriter/support/doceditor/state/nv_editor_state.py
+++ markupwriter/support/doceditor/state/nv_editor_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import annotations
+
 import re
 
 from PyQt6.QtCore import (
@@ -22,6 +24,98 @@ from markupwriter.common.provider import Key
 import markupwriter.support.doceditor.state as s
 
 
+class BaseBufferState(object):
+    def __init__(self, state: NvEditorState) -> None:
+        self.state = state
+
+    def enter(self, ckey: str):
+        raise NotImplementedError()
+
+    def process(self, ckey: str):
+        raise NotImplementedError()
+
+
+class InitBufferState(BaseBufferState):
+    def __init__(self, state: NvEditorState) -> None:
+        super().__init__(state)
+
+    def enter(self):
+        self.state.reset()
+
+    def process(self, ckey: str):
+        # Count
+        if self.state.countRegex.search(ckey) is not None:
+            self.state.setBufferState(CountBufferState(self.state))
+        # Prefix
+        elif self.state.prefixRegex.search(ckey) is not None:
+            self.state.setBufferState(PrefixBufferState(self.state))
+        # Operator
+        elif self.state.opRegex.search(ckey) is not None:
+            self.state.setBufferState(OperatorBufferState(self.state))
+        # Command
+        elif self.state.commandRegex.search(ckey) is not None:
+            self.state.setBufferState(CommandBufferState(self.state))
+
+
+class CountBufferState(BaseBufferState):
+    def __init__(self, state: NvEditorState) -> None:
+        super().__init__(state)
+
+    def enter(self, ckey: str):
+        self.state.buffer += ckey
+
+    def process(self, ckey: str):
+        # Count
+        if ckey.isnumeric():
+            self.state.buffer += ckey
+        # Prefix
+        elif self.state.prefixRegex.search(ckey) is not None:
+            self.state.setBufferState(PrefixBufferState(self.state))
+        # Operator
+        elif self.state.opRegex.search(ckey) is not None:
+            self.state.setBufferState(OperatorBufferState(self.state))
+
+
+class PrefixBufferState(BaseBufferState):
+    def __init__(self, state: NvEditorState) -> None:
+        super().__init__(state)
+
+    def enter(self, ckey: str):
+        pass
+
+    def process(self, ckey: str):
+        sequence = self.state.prevCKey + ckey
+        
+        # Operator
+        if self.state.opRegex.search(sequence) is not None:
+            self.state.setBufferState(OperatorBufferState(self.state))
+        # Invalid
+        else:
+            self.state.setBufferState(InitBufferState(self.state))
+
+
+class OperatorBufferState(BaseBufferState):
+    def __init__(self, state: NvEditorState) -> None:
+        super().__init__(state)
+
+    def enter(self, ckey: str):
+        pass
+    
+    def process(self, ckey: str):
+        pass
+    
+    
+class CommandBufferState(BaseBufferState):
+    def __init__(self, state: NvEditorState) -> None:
+        super().__init__(state)
+        
+    def enter(self, ckey: str):
+        pass
+    
+    def process(self, ckey: str):
+        pass
+    
+
 class NvEditorState(s.BaseEditorState):
     def __init__(
         self,
@@ -32,18 +126,19 @@ class NvEditorState(s.BaseEditorState):
         super().__init__(editor, parent)
 
         self.buffer = ""
-        self.hasPrefix = False
-        self.hasOp = False
+        self.prevCKey = ""
+        self.currCKey = ""
         self.moveMode = moveMode
-        
-        self.countRegex = re.compile(r"[1-9]+")
+        self.bufferState: BaseBufferState = InitBufferState(self)
+
+        self.countRegex = re.compile(r"[1-9][0-9]*")
         self.prefixRegex = re.compile(r"\bnone\b")
         self.opRegex = re.compile(r"\bnone\b")
         self.motionRegex = re.compile(r"\bnone\b")
         self.commandRegex = re.compile(r"\bnone\b")
-        
+
         self.opDict: dict[str, function] = dict()
-        
+
     def enter(self):
         raise NotImplementedError()
 
@@ -52,159 +147,60 @@ class NvEditorState(s.BaseEditorState):
 
     def reset(self):
         self.buffer = ""
-        self.hasPrefix = False
-        self.hasOp = False
-    
+        self.prevCKey = ""
+        self.currCKey = ""
+
     def process(self, e: QKeyEvent) -> bool:
-        if e.key() == Qt.Key.Key_Escape:
-            if self.buffer != "":
-                self.reset()
-                return True
-
-        if self.build(e):
-            self.evaluate()
-            self.reset()
+        self.prevCKey = self.currCKey
+        self.currCKey = Key.get(e.modifiers(), e.key())
+        self.bufferState.process(self.currCKey)
+        print(self.buffer)
 
         return True
-        
-    def build(self, e: QKeyEvent):
-        # Ignore modifier keys
-        isShift = Qt.Key.Key_Shift == e.key()
-        isCtrl = Qt.Key.Key_Control == e.key()
-        isAlt = Qt.Key.Key_Alt == e.key()
-        if isShift or isCtrl or isAlt:
-            return False
-        
-        # Get the converted key.
-        ckey: str = Key.get(e.modifiers(), e.key())
-        if ckey is None:
-            self.reset()
-            return False
 
-        # Build the buffer.
-        self.buffer += ckey
-
-        # Found count. Continue building the buffer.
-        countFound = self.countRegex.search(ckey)
-        if countFound is not None:
-            return False
-
-        # Previous ckey was a prefix. Check if we now
-        # have a valid operator or command to process.
-        if self.hasPrefix:
-            hasOp = self.opRegex.search(self.buffer) is not None
-            hasCmd = self.commandRegex.search(self.buffer) is not None
-            if not (hasOp or hasCmd):
-                self.reset()
-                return False
-            self.hasPrefix = False
-        # Check if this ckey is a prefix to a operator
-        # or command.
-        else:
-            prefixFound = self.prefixRegex.search(ckey)
-            if prefixFound is not None:
-                self.hasPrefix = True
-                return False
-            
-        # A command was entered after an operator,
-        # so process it.
-        if self.hasOp:
-            # Make sure we aren't processing a prefixed
-            # command.
-            if not self.hasPrefix:
-                return True
-
-        # Check if we have a valid operator.
-        opFound = self.opRegex.search(self.buffer)
-        if opFound is not None:
-            self.hasOp = True
-            return False
-
-        # Check to see if the command is valid,
-        # if not then reset the buffer.
-        cmdFound = self.commandRegex.search(self.buffer)
-        if cmdFound is None:
-            self.reset()
-            return False
-
-        return True
-    
-    def evaluate(self):
-        # Parse the count
-        count = 1
-        it = self.countRegex.finditer(self.buffer)
-        for found in it:
-            num = int(found.group(0))
-            count *= num
-            self.buffer = self.buffer.replace(found.group(0), "", 1)
-
-        # Parse the command
-        cmd = None
-        found = self.commandRegex.search(self.buffer)
-        if found is not None:
-            cmd = found.group(0)
-        else:
-            return
-
-        # Parse the operator
-        op = None
-        if self.hasOp:
-            found = self.opRegex.search(self.buffer)
-            op = found.group(0)
-            
-            # Make sure we are operating on a 
-            # motion command.
-            found = self.motionRegex.search(cmd)
-            if found is None:
-                return
-            
-            self.moveMode = QTextCursor.MoveMode.KeepAnchor
-
-        for _ in range(count):
-            self.funcDict[cmd]()
-
-        if op is not None:
-            self.opDict[op]()
+    def setBufferState(self, state: BaseBufferState):
+        self.bufferState = state
+        self.bufferState.enter(self.currCKey)
 
     def _b(self):
         doc = self.editor.document()
         cursor = self.editor.textCursor()
         flag = QTextDocument.FindFlag.FindBackward
         cursor.movePosition(QTextCursor.MoveOperation.Left, self.moveMode)
-        
+
         newCursor = doc.find(" ", cursor.position(), flag)
         newCursor.movePosition(QTextCursor.MoveOperation.Right)
         cursor.setPosition(newCursor.position(), self.moveMode)
-        
+
         self.editor.setTextCursor(cursor)
 
     def _e(self):
         doc = self.editor.document()
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Right, self.moveMode)
-        
+
         newCursor = doc.find(" ", cursor.position())
         newCursor.movePosition(QTextCursor.MoveOperation.Left)
         cursor.setPosition(newCursor.position(), self.moveMode)
-        
+
         self.editor.setTextCursor(cursor)
 
     def _ge(self):
         doc = self.editor.document()
         cursor = self.editor.textCursor()
         flag = QTextDocument.FindFlag.FindBackward
-        
+
         newCursor = doc.find(" ", cursor.position(), flag)
         newCursor.movePosition(QTextCursor.MoveOperation.Left)
         cursor.setPosition(newCursor.position(), self.moveMode)
-            
+
         self.editor.setTextCursor(cursor)
 
     def _h(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Left, self.moveMode)
         self.editor.setTextCursor(cursor)
-        
+
     def _j(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Down, self.moveMode)
@@ -219,39 +215,38 @@ class NvEditorState(s.BaseEditorState):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Right, self.moveMode)
         self.editor.setTextCursor(cursor)
-        
+
     def _w(self):
         doc = self.editor.document()
         cursor = self.editor.textCursor()
-        
+
         newCursor = doc.find(" ", cursor.position())
         newCursor.movePosition(QTextCursor.MoveOperation.Right)
         cursor.setPosition(newCursor.position(), self.moveMode)
-        
+
         self.editor.setTextCursor(cursor)
-        
+
     def _x(self):
         cursor = self.editor.textCursor()
         cursor.deleteChar()
         self.editor.setTextCursor(cursor)
-        
+
     def _0(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.StartOfLine, self.moveMode)
         self.editor.setTextCursor(cursor)
-    
+
     def _dollar(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.EndOfLine, self.moveMode)
         self.editor.setTextCursor(cursor)
-    
+
     def _C_D(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Down, self.moveMode, 25)
         self.editor.setTextCursor(cursor)
-    
+
     def _C_U(self):
         cursor = self.editor.textCursor()
         cursor.movePosition(QTextCursor.MoveOperation.Up, self.moveMode, 25)
         self.editor.setTextCursor(cursor)
-    
