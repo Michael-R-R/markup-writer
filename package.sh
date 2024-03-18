37daf14fcf87ee4c9c9cc7fd1f29d4db0467101b
Michael-R-R <mikerule68@gmail.com>
2024-03-18 12:53:49 -0700

    updated packaging

--- package.sh
+++ package.sh
@@ -7,12 +7,12 @@ mkdir -p package/usr/share/applications
 mkdir -p package/usr/share/icons/hicolor/scalable/apps
 
 # Copy files (change icon names, add lines for non-scaled icons)
-cp -r dist/MarkupWriter package/opt/MarkupWriter
-cp resources/icons/common/books.svg package/usr/share/icons/hicolor/scalable/apps/MarkupWriter.svg
-cp MarkupWriter.desktop package/usr/share/applications
+cp -r dist/markup-writer package/opt/markup-writer
+cp resources/icons/common/books.svg package/usr/share/icons/hicolor/scalable/apps/markup-writer.svg
+cp markup-writer.desktop package/usr/share/applications
 
 # Change permissions
-find package/opt/MarkupWriter -type f -exec chmod 644 -- {} +
-find package/opt/MarkupWriter -type d -exec chmod 755 -- {} +
+find package/opt/markup-writer -type f -exec chmod 644 -- {} +
+find package/opt/markup-writer -type d -exec chmod 755 -- {} +
 find package/usr/share -type f -exec chmod 644 -- {} +
-chmod +x package/opt/MarkupWriter/MarkupWriter
+chmod +x package/opt/markup-writer/markup-writer
